dc0c14b9d5353ca1ddc120bcb8eeedb25518edef
chaechaeyeon <00dreamadream@kakao.com>
2023-12-16 01:31:03 +0900

    [Feat]commList

--- Fitbuddy/src/main/java/controller/comm/ListCommunityController.java
+++ Fitbuddy/src/main/java/controller/comm/ListCommunityController.java
@@ -3,25 +3,28 @@ package controller.comm;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import controller.Controller;
 import model.Community;
+
 import model.service.UserManager;
 
 public class ListCommunityController implements Controller {
+	
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // 세션에서 사용자 ID 가져오기
-        HttpSession session = request.getSession();
-        Object userId = session.getAttribute("userId");
-
-        // UserManager를 통해 커뮤니티 리스트 가져오기
-        UserManager manager = UserManager.getInstance();
-        List<Community> commList = manager.findCommunityList();
-
-        // commList 객체를 request에 저장하여 커뮤니티 리스트 화면으로 이동(forwarding)
-        request.setAttribute("commList", commList);
-        return "/comm/comm.jsp";
+    public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+    	// 커뮤니티 게시글 목록
+    	UserManager manager = UserManager.getInstance();
+		List<Community> commList = manager.findCommunityPostList();
+		
+//		for (Community comm : commList) {
+//		    int userId = comm.getCmUserId();
+//		    String nickname = manager.findnameByUserId(userId);
+//		    if (nickname != null) {
+//		        comm.setUserName(nickname);
+//		    }
+//		}
+		
+		request.setAttribute("commList", commList);				
+		return "/comm/commList.jsp";       
     }
 }
---
+++ Fitbuddy/src/main/java/model/dao/CommentDao.java
@@ -0,0 +1,14 @@
+// CommentDao.java
+package model.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import model.Comment;
+
+public class CommentDao {
+    
+    public Comment create(Comment comment) throws SQLException {
+        
+        return comment;  
+    }
+}
